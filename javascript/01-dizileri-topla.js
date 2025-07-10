2a2a941bab84795be93ed7c23455db08c8465e0d
Furkan Albayrak <157476431+Furkannalbayrak@users.noreply.github.com>
2025-07-10 10:27:12 +0300

    Update 01-dizileri-topla.js

--- javascript/01-dizileri-topla.js
+++ javascript/01-dizileri-topla.js
@@ -1,1 +1,21 @@
+// 1. Yöntem
 
+function sumNumbers(numbers) {
+  if (numbers.length === 0) {
+    return 0;
+  }
+
+  let toplam = 0;
+
+  for (let i = 0; i < numbers.length; i++) {
+    toplam += numbers[i];
+  }
+
+  return toplam;
+}
+
+// 2. Yöntem
+
+function sumNumbers(numbers) {
+   return numbers.reduce((a, b) => a + b, 0);
+}
